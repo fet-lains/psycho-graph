0a58f34dc25b03d3bc3dba17360c2eb56df17946
Fet Lains <f.sharaputo@inetstudio.ru>
2025-01-14 19:28:33 +0300

    add graph & data

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
+import { data } from './data.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   (async function () {
-    const data = [
-      { year: 2010, count: 10 },
-      { year: 2011, count: 20 },
-      { year: 2012, count: 15 },
-      { year: 2013, count: 25 },
-      { year: 2014, count: 22 },
-      { year: 2015, count: 30 },
-      { year: 2016, count: 28 },
-    ];
-
     new Chart(document.getElementById('graph'), {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: data.map((row) => row.year),
+        labels: data.map((row) => row.week),
         datasets: [
           {
-            label: 'Acquisitions by year',
-            data: data.map((row) => row.count),
+            label: 'Качество сна',
+            data: data.map((row) => row.sleepQuality),
+          },
+          {
+            label: 'Тревожность',
+            data: data.map((row) => row.anxiety),
+          },
+          {
+            label: 'Подавленность',
+            data: data.map((row) => row.depression),
+          },
+          {
+            label: 'Утомляемость',
+            data: data.map((row) => row.fatigue),
+          },
+          {
+            label: 'Негативные переживания',
+            data: data.map((row) => row.negativeThoughts),
+          },
+          {
+            label: 'Озабоченность ЖКТ',
+            data: data.map((row) => row.gutConcerns),
           },
         ],
       },
